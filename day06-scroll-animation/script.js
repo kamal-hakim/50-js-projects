99a216ef6d8354baa858f5a5068a983634292c4f
Kamal Hakim <kamalhakim.zainal@gmail.com>
2022-01-14 09:31:13 +0800

    Created day06 project

---
+++ day06-scroll-animation/script.js
@@ -0,0 +1,17 @@
+const box = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes()
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 4;
+  
+  box.forEach(box => {
+    if(box.getBoundingClientRect().top < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  })
+}
